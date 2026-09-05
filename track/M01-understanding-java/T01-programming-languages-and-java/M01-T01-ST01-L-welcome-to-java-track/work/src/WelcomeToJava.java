2e8a056431473ffbae24cfb99b4def6141c2e039
adwitiya584 <mohitppandey098@gmail.com>
2026-09-05 19:29:07 +0530

    this is second progrma

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,7 +1,5 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
-       System.out.println("Welcome to the Java track!");
-    
+    System.out.println("Welcome to the Java track!"); 
     }
 }
